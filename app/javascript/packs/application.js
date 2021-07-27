8c300092a40729d143053de76dc8d17b445ff58b
Omar Labana <omarlabana@outlook.com>
2021-07-27 07:01:25 +0200

    Fix Tailwind CSS config

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,7 +8,7 @@ import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 
-import 'stylesheets/application'
+import "stylesheets/application"
 
 Rails.start()
 Turbolinks.start()
