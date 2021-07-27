0bed0ea55069a0653fadb545236802b772036ede
Omar Labana <omarlabana@outlook.com>
2021-07-27 07:30:30 +0200

    Edit login logic for users

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,5 +9,23 @@ class User < ApplicationRecord
   has_many :articles
   has_many :votes
   
-  
+  attr_writer :login
+
+  def login
+    @login || username || email
+  end
+
+  def none_existing
+    errors.add(:username, :invalid) if User.where(email: username).exists?
+  end
+
+  def self.find_for_database_authentication(warden_conditions)
+    conditions = warden_conditions.dup
+    if (login = conditions.delete(:login))
+      where(conditions.to_h).where(['lower(username) = :value OR lower(email) = :value',
+                                    { value: login.downcase }]).first
+    elsif conditions.key?(:username) || conditions.key?(:email)
+      where(conditions.to_h).first
+    end
+  end
 end
