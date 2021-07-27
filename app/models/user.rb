2764a7fdac8720c001eff0c475a898a17b6993ec
Omar Labana <omarlabana@outlook.com>
2021-07-27 07:29:51 +0200

    Add validations to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
-  
+  # Validations
+  validate :none_existing
+  validates :username, presence: true
+  validates_uniqueness_of :username
 
   # Associations
   has_many :articles
