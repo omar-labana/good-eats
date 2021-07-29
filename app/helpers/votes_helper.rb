decb845f842063c28743d702ff2c33091bec7a13
Omar Labana <omarlabana@outlook.com>
2021-07-29 04:52:37 +0200

    Add vote controller

---
+++ app/helpers/votes_helper.rb
@@ -0,0 +1,7 @@
+module VotesHelper
+    def most_voted?
+      Vote.where(user_id: current_user.id, article_id:
+      params[:article_id]).exists?
+    end
+  end
+  
--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,2 +1,7 @@
 class Vote < ApplicationRecord
-end
+    belongs_to :article
+    belongs_to :user
+  
+    scope :most_voted, -> { group(:article_id).order('count_id DESC').count('id') }
+  end
+  
