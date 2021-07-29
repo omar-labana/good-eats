910bbbd3ff9322d751820fee6d56e54662321cb2
Omar Labana <omarlabana@outlook.com>
2021-07-29 05:02:06 +0200

    Fix association error between category and article

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,2 +1,8 @@
 class Category < ApplicationRecord
-end
+    has_many :articles
+
+    validates :name, presence: true, length: { maximum: 20, too_long: '20 characters in post is the maximum allowed.' }
+    
+    scope :order_by_priority, -> { order(priority: :asc) }
+  end
+  
